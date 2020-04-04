5bf8406c7a56b31c8f6a4cde58dde52af0df7ebb
Vandit Patel <patel.vandit1@gmail.com>
2020-04-04 14:28:09 -0600

    added package updates, bootstrap CDN and a sample card

---
+++ src/character_cards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+function Duke(props) {
+
+    return (
+        <Card br='primary' style={{ width: '18rem' }}>
+            Duke...
+        </Card>
+    );
+}
+
+export default Duke;
