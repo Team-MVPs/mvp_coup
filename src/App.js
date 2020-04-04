fc616be1e2b912e98ae603f79d18bc2cce32c51c
Vandit Patel <patel.vandit1@gmail.com>
2020-04-04 15:47:43 -0600

    Aded flow lmao

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import './App.css';
 import Duke from './character_cards.js';
--- src/character_cards.js
+++ src/character_cards.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import {Card} from 'react-bootstrap';
 
