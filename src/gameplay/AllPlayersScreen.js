67dd17b2f48ef25849c2cd5f998306f9948d2401
Vandit Patel <patel.vandit1@gmail.com>
2020-04-05 00:17:03 -0600

    sample character cards

--- src/gameplay/AllPlayersScreen.js
+++ src/gameplay/AllPlayersScreen.js
@@ -1,8 +1,21 @@
 // @flow
 import React from 'react';
+import Character from '../characters/Character.js';
+
+const all_chars = ["Duke", "Assassin", "Contessa", "Captain", "Ambassador"];
+
+const chars = all_chars.map((name, inx) => (
+    <Character name={name} key={inx} show_card={false}/>
+));
 
 function PlayerScreen(props) {
-    return (<div align="center">Middle Part</div>);
+    return (
+        <div align="center">
+            Face Down Cards
+            <br></br>
+            {chars}
+        </div>
+    );
 }
 
 export default PlayerScreen;
--- src/gameplay/UserDetails.js
+++ src/gameplay/UserDetails.js
@@ -1,9 +1,21 @@
 // @flow
 import React from 'react';
+import Character from '../characters/Character.js';
+
+
+const all_chars = ["Duke", "Assassin", "Contessa", "Captain", "Ambassador"];
+
+const chars = all_chars.map((name, inx) => (
+    <Character name={name} key={inx} show_card={true}/>
+));
 
 function UserDetails(props) {
     return (
-        <div align="center">Sidebar Left</div>
+        <div align="center">
+            Face Up Cards
+            <br></br>
+            {chars}
+        </div>
     );
 }
 
