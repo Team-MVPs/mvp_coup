33e1bee515c43e989a09a4dfa07d68f1a693914f
Aravind Segu <aravind@Aravinds-MacBook-Pro.local>
2020-04-05 00:27:40 -0400

    Firebase Stuff

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@
 
 import React from 'react';
 import './App.css';
+import {firestore} from './config/firebase';
 // import Duke from './character_cards.js';
 import LoginComponent from "./LoginComponent";
 
 
 function App() {
+  firestore.collection('testing').doc("Aravind")
+      .onSnapshot((snapshot) => {
+        console.log(snapshot.data());
+      }, (error) => console.error(error));
+
   return (
       <LoginComponent />
   );
--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from 'firebase';
+import {firestore} from './config/firebase';
 
 class LoginComponent extends React.Component {
 	constructor(props) {
@@ -25,22 +25,8 @@ class LoginComponent extends React.Component {
 		
 		event.preventDefault();
 		
-		const firebaseConfig = {
-			apiKey: "AIzaSyAM_2cfUP2u2pv55y_WgfnXrzO16l4RtVQ",
-			authDomain: "coup-c4862.firebaseapp.com",
-			databaseURL: "https://coup-c4862.firebaseio.com",
-			projectId: "coup-c4862",
-			storageBucket: "coup-c4862.appspot.com",
-			messagingSenderId: "1018352808422",
-			appId: "1:1018352808422:web:e597c6b7985c0a39e52627",
-			measurementId: "G-MDTYYPRVDF"
-		};
-		// Initialize Firebase
-		firebase.initializeApp(firebaseConfig);
-		firebase.analytics();
 		
-		const db = firebase.firestore();
-		const userRef = db.collection("testing").add({
+		const userRef = firestore.collection("testing").doc("Aravind").set({
 			key1: this.state.value,
 			key2: "sample"
 		});
