dcfd22f800271908a10d06e8725df2f795e61e42
Shivam Dharme <shdh8888@yahoo.ca>
2020-04-06 13:27:05 -0600

    Refactor login component

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -2,49 +2,30 @@ import React from 'react';
 import { register } from '../backend/startup';
 import { Redirect } from 'react-router-dom'
 
+function LoginComponent(props) {
+	const [value, setValue] = React.useState("");
+	const [redirect, setRedirect] = React.useState(false);
 
-// Why is this a class :(
-// I'm BIG MAD
-
-class LoginComponent extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			value: '',
-			redirect: false
-		};
-
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	handleChange(event) {
-		this.setState({ value: event.target.value });
-	}
-
-	handleSubmit(event) {
+	const handleChange = (event) => { setValue(event.target.value) }
+	const handleSubmit = (event) => {
 		event.preventDefault();
-	    register(this.state.value);
-		this.setState({ redirect: true });
+		register(value);
+		setRedirect(true);
 	}
 
-	render() {
-		if (this.state.redirect) {
-			return (<Redirect to="/start" />)
-		} else {
-
-			return (
-				<form onSubmit={this.handleSubmit.bind(this)}>
-					<label>
-						Name:
-						<input type="text" value={this.state.value} onChange={this.handleChange} />
-					</label>
-					<input type="submit" value="Submit" />
-				</form>
-			);
-		}
+	if (redirect) {
+		return (<Redirect to="/start" />)
 	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Name:
+				<input type="text" value={value} onChange={handleChange} />
+			</label>
+			<input type="submit" value="Submit" />
+		</form>
+	)
 }
 
 export default LoginComponent;
