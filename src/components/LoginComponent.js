0665118c31fb7287c348fad5aa1838f353e062bf
aravind-segu <yashashvi99@yahoo.ca>
2020-04-09 21:38:34 -0400

    Room Screen

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,40 +1,88 @@
 import React from 'react';
-import { register } from '../backend/startup';
+import { register, checkRoomNameExists, createRoomName } from '../backend/startup';
 import { Redirect } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function LoginComponent(props) {
-	const [value, setValue] = React.useState("");
+	const [playerName, setPlayerName] = React.useState("");
+	const [roomName, setRoomName] = React.useState("");
 	const [redirect, setRedirect] = React.useState(false);
 
-	const handleChange = (event) => { setValue(event.target.value) }
-	const handleSubmit = (event) => {
+	const handlePlayerNameChange = (event) => { setPlayerName(event.target.value) }
+	const handleRoomNameChange = (event) => { setRoomName(event.target.value) }
+
+	const newRoom = (event) => {
 		event.preventDefault();
-		if (value.length === 0){
-			alert('Please input a Name');
-			return (<Redirect to = "/" />);
-		} else{
-			register(props.setPlayerID, value);
-			setRedirect(true);			
-		}		
+		if (playerName.length === 0) {
+			alert('Please input a Player Name');
+			return (<Redirect to="/" />);
+		} else if (roomName.length === 0) {
+			alert('Please enter a Room name');
+			return (<Redirect to="/" />);
+		} else {
+			checkRoomNameExists(roomName).then((exists) => {
+				if (exists) {
+					alert('Room name already exists');
+					return (<Redirect to="/" />);
+				} else {
+					createRoomName(roomName).then(() => {
+						register(props.setPlayerID, playerName, roomName).then(() => {
+							props.setHost(true);
+							props.setRoomName(roomName);
+							setRedirect(true);
+						})
+					})
+				}
+			})
+		}
+	}
+
+	const existingRoom = (event) => {
+		console.log("In Existing Room");
+		event.preventDefault();
+		if (playerName.length === 0) {
+			alert('Please input a Player Name');
+			return (<Redirect to="/" />);
+		} else if (roomName.length === 0) {
+			alert('Please enter a Room name');
+			return (<Redirect to="/" />);
+		} else {
+			checkRoomNameExists(roomName).then((exists) => {
+				if (!exists) {
+					alert('Room name does not exist');
+					return (<Redirect to="/" />);
+				} else {
+					register(props.setPlayerID, playerName, roomName).then(() => {
+						props.setRoomName(roomName);
+						setRedirect(true);
+					})
+				}
+			})
+		}
 	}
 
 	if (redirect) {
-		//return (<Redirect to="/start" />)
 		return (
 			<Redirect to="/GameStart" />)
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className = "form=group">
-				<label style = {{marginTop:50}}>
-					<input className ="form-control" type="text" value={value} onChange={handleChange} placeholder = "Enter Name"/>
-				</label>
-			</div>
-			<div>
-				<button type="Submit" className="btn btn-primary mb-2" onClick = {handleSubmit}>Submit</button>
-			</div>
-		</form>
+		<Container style={{ marginTop: "22%", width: "50%" }}>
+			<Row style={{ marginBottom: "3%" }}>
+				<input className="form-control" type="text" value={playerName} onChange={handlePlayerNameChange} placeholder="Player Name" />
+			</Row>
+			<Row style={{ marginBottom: "3%" }}>
+				<input className="form-control" type="text" value={roomName} onChange={handleRoomNameChange} placeholder="Room Name" />
+			</Row>
+			<Row>
+				<Col xs={6}>
+					<button style={{ width: "100%" }} type="Submit" className="btn btn-primary mb-2" onClick={newRoom}>Create a New Room</button>
+				</Col>
+				<Col xs={6}>
+					<button style={{ width: "100%" }} type="Submit" className="btn btn-primary mb-2" onClick={existingRoom}>Join an Existing Room</button>
+				</Col>
+			</Row>
+		</Container>
 	)
 }
 
