d2a2c1cbe0e82f9f71d4294a101848e1f99a6f28
Preet Shah <preettshah@hotmail.com>
2020-04-10 14:02:53 -0600

    added in Leave Room button and start game can now only be activated when more than one player in lobby

--- src/gameplay/WaitForHost.js
+++ src/gameplay/WaitForHost.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { firestore } from '../config/firebase';
 import { Redirect } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
 
 function WaitForHost(props){
+    const [leave, setLeave] = React.useState(false);
+
+    const handleLeaveRoom = (event) =>{
+      setLeave(true);
+    }
+
     const [gameStarted, setStart] = React.useState(false);
     React.useEffect(() => {
         const subscribe = firestore.collection("root").doc(props.roomName).onSnapshot((doc) => {
@@ -13,11 +21,26 @@ function WaitForHost(props){
         })
         return () => subscribe();
       }, []);
-    
+      
+      if(leave){
+        firestore.collection("root").doc(props.roomName).collection("players").doc(props.id).delete().then(()=>{
+        console.log('A player left ' + props.roomName);
+        })
+        return (<Redirect to ="/" />);
+      }
+
+
       if(gameStarted){
         return (<Redirect to="/start" />);
       }else{
-          return(<div style={{fontSize:"large"}}>Waiting on Host to Start the Game ...</div>)
+          return(
+            <div>
+              <div style={{fontSize:"large", marginBottom: 30}}>
+                <h3>Waiting on Host to Start the Game ...</h3>
+              </div>
+              <button className="btn btn-lg btn-primary" onClick = {handleLeaveRoom}>Leave Room</button>
+            </div>
+            )
       }
 }
 
