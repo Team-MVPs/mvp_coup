480ef16f7dab0cc686619e1f53cc15b492b86bbf
aravind-segu <yashashvi99@yahoo.ca>
2020-04-09 21:38:34 -0400

    Room Screen

--- src/backend/startup.js
+++ src/backend/startup.js
@@ -4,25 +4,51 @@ import { firestore } from '../config/firebase';
 //var roomName = "vip";
 var roomName = "Preet Testing"
 var playerID = "";
+var root = "root";
 
-export async function register(setPlayerID, name) {
-    await firestore.collection(roomName).add({
+export async function register(setPlayerID, name, roomName) {
+    await firestore.collection(root).doc(roomName).collection("players").add({
         name: name,
         count: 0,
         id: 0
     })
-    .then(function(docRef) {
-        playerID = docRef.id;
-        setPlayerID(playerID);
-        console.log("Document written with ID: ", playerID);
+        .then(function (docRef) {
+            playerID = docRef.id;
+            setPlayerID(playerID);
+            console.log("Document written with ID: ", playerID);
+        })
+    firestore.collection(roomName).onSnapshot((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            let docId = doc.id;
+            firestore.collection(roomName).doc(docId).update({
+                id: docId
+            });
+        })
     })
-    firestore.collection(roomName).onSnapshot((snapshot)=>{
-    	snapshot.docs.forEach((doc)=>{
-    		let docId = doc.id;
-    		firestore.collection(roomName).doc(docId).update({
-    			id: docId
-    		});
-     	})
-    })
+}
+
+export async function checkRoomNameExists(roomName) {
+    console.log("Checking");
+    const snapshot = await firestore.collection(root).get();
+    for(let i = 0; i < snapshot.docs.length; i++){
+        console.log(snapshot.docs[i].id);
+        if (snapshot.docs[i].id === roomName) {
+            console.log("Returning True");
+            return true;
+        }
+    }
+    return false;
+}
+
+export async function createRoomName(roomName) {
+    await firestore.collection(root).doc(roomName).set({
+        startGame: false
+    });
+}
+
+export async function startGame(roomName){
+    await firestore.collection(root).doc(roomName).set({
+        startGame: true
+    });
 }
 
