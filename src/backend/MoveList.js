28ffebfd6b917f03151222bdc8b90fb5bfb9024c
Preet Shah <preettshah@hotmail.com>
2020-04-20 15:05:54 -0600

    movelist is changed:

--- src/backend/MoveList.js
+++ src/backend/MoveList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import {all_moves, responses} from './move_logic.js';
+import {all_moves, responses, updateTurnInDB} from './move_logic.js';
+
 
 
 export function MoveList(props){
-	return (<div>
+	/*return (<div>
 		{Object.keys(all_moves).map(move => (
 			<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 				<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves[move](props.roomName, props.currentTurn, props.playerName, props.playerID)}>{move}</Button>
+			</div>
+		))}
+	</div>);*/
+	return (<div>
+		{Object.keys(all_moves).map(move => (
+			<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
+				<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={updateTurnInDB(props.roomName, props.currentTurn, props.playerName, props.playerID, move)}>{move}</Button>
 			</div>
 		))}
 	</div>);
