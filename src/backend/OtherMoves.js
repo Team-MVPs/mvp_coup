3069a91929b359df4f15c1c44be8b3d2d3c09f0e
Preet Shah <preettshah@hotmail.com>
2020-04-22 12:01:01 -0600

    final changes

--- src/backend/OtherMoves.js
+++ src/backend/OtherMoves.js
@@ -3,16 +3,21 @@ import {handleDBException} from "./callbacks";
 import firebase from 'firebase';
 import React, {useContext, useEffect, useState} from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import { incrementTurn } from './move_logic';
 
 
-function Ambassador(roomName, playerID){
+function Ambassador(roomName, playerID, setChangeTurn){
 	const [cards, setCards] = useState([]);
 	const [cardDeck, setCardDeck] = useState([]);
+	const [isDisabled, setDisabled] = useState(false);
+
+	let chosenCards = [];
+	let chosenKeys = [];
 
 	useEffect(()=>{
 		const subscribe = firestore.collection(root).doc(roomName).get().then(async (room)=>{
 			let viewCards = [];
-			viewCards.push(room.data().cards[0], room.data().cards[1]);
+			viewCards.push([room.data().cards[0], 1], [room.data().cards[1], 2]);
 			let allCards = room.data().cards;
 			allCards.shift();
 			allCards.shift();
@@ -20,9 +25,9 @@ function Ambassador(roomName, playerID){
 
 			await firestore.collection(root).doc(roomName).collection("players").doc(playerID).get().then((player)=>{
 				if (player.data().cards.length >1){
-					viewCards.push(player.data().cards[0], player.data().cards[1]);
+					viewCards.push([player.data().cards[0], 3], [player.data().cards[1], 4]);
 				} else{
-					viewCards.push(player.data().cards[0]);
+					viewCards.push([player.data().cards[0], 3]);
 				}
 			})
 		//console.log(viewCards);
@@ -31,8 +36,48 @@ function Ambassador(roomName, playerID){
 		return () => subscribe();
 	}, []);
 
-	const handleClick = (card) =>{
-		console.log("pressed card");
+	const handleClick = (card) => {
+		return async () => {
+			console.log(card);
+			if(chosenCards.length === 0){
+				chosenCards.push(card[0]);
+				chosenKeys.push(card[1]);
+			} else{
+				if (chosenKeys[0] !== card[1]){
+					chosenCards.push(card[0]);
+					chosenKeys.push(card[1]);
+					
+
+				} else{
+					alert("You have already selected that card, pick another one")
+				}
+				if (chosenCards.length > 1){
+					setDisabled(true)
+					await firestore.collection(root).doc(roomName).collection("players").doc(playerID).update({
+						cards: chosenCards
+					});
+					let oldCards = cardDeck;
+					for(let i=0; i<cards.length;i++){
+						let found = false;
+						for(let j=0; j<chosenKeys.length;j++){
+							if (chosenKeys[j] === cards[i][1]){
+								found = true;
+							}
+						}
+						if(!found){
+							oldCards.push(cards[i][0]);
+							console.log('called' + cards[i][0]);
+						}
+					}
+					setCardDeck(oldCards);
+					await firestore.collection(root).doc(roomName).update({
+						cards: cardDeck
+					});
+					setChangeTurn(true);
+				}
+			}
+					
+		}
 	}
 
 	return (
@@ -40,9 +85,11 @@ function Ambassador(roomName, playerID){
 			<ul>
 				{cards.map(card =>(
 					<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
-						<button type="button" className="btn btn-lg btn-primary" key = {card} style = {{width:"20em"}} onClick = {handleClick(card)}>{card} </button>
+						<button type="button" className="btn btn-lg btn-primary" key = {card} style = {{width:"20em"}} 
+						onClick = {handleClick(card)} disabled = {isDisabled}>{card[0]} </button>
 					</div>))}
 			</ul>
+
 		</div>)
 
 }
@@ -53,11 +100,16 @@ export default function OtherMoves(props){
 	let move = props.move;
 	let roomName = props.roomName;
 	let playerID = props.playerID;
+	const [changeTurn, setChangeTurn] = useState(false);
 
 	if (move === 'Ambassador'){
+		if (changeTurn){
+			incrementTurn(roomName);
+		}
+
 		return (
 			<div>
-				{Ambassador(roomName, playerID)}
+				{Ambassador(roomName, playerID, setChangeTurn)}
 			</div>)
 	}		
 }
