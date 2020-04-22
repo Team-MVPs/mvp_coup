c0e822564187dae9e451469c35dfcafe44aefcfd
Preet Shah <preettshah@hotmail.com>
2020-04-22 17:27:09 -0600

    button changes

--- src/backend/MoveList.js
+++ src/backend/MoveList.js
@@ -9,51 +9,57 @@ export function MoveList(props){
 	const [assassinEnabled, setAssassin] = useState(true);
 	const [coupEnabled, setCoup] = useState(true);
 	const [theRest, setTheRest] = useState(false);
-	const [coins, setCoins] = useState(0);
 
 	useEffect( () => {
-		firestore.collection(root).doc(props.roomName).collection("players").doc(props.activePlayerID).get().then((player)=>{
-			console.log("HEEEEEEEEEEEEEEEEE");
-			setCoins(player.data().coins);
-			console.log("REEEEEEEEEEEEEEEEE")
-		});
-	}
+		const subscribe = firestore.collection(root).doc(props.roomName).collection("players").doc(props.activePlayerID).get().then((player)=>{
+			let coins = player.data().coins;
+			if (coins >= 3 && coins < 10){
+				setAssassin(false);
+			} 
+			if (coins >= 7){
+				setCoup(false);
+			}
+			if (coins >= 10){
+				setTheRest(true);
+			}
+			});
+		return () => subscribe; 
+		}, []);
 
-	);
 	return (<div>
 				<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 					<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves["Take General Income"](props.roomName, props.currentTurn, props.playerName, props.activePlayerID)}
-					disabled={false}>Take General Income</Button>
+					disabled={theRest}>Take General Income</Button>
 				</div>
 
 				<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 					<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves["Take Foreign Aid"](props.roomName, props.currentTurn, props.playerName, props.activePlayerID)}
-					disabled = {false}>Take Foreign Aid</Button>
+					disabled = {theRest}>Take Foreign Aid</Button>
 				</div>
 
 				<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 					<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves["Take 3 as Duke"](props.roomName, props.currentTurn, props.playerName, props.activePlayerID)}
-					disabled = {false}>Take 3 as Duke</Button>
+					disabled = {theRest}>Take 3 as Duke</Button>
 				</div>
 
 				<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 					<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves["Exchange your cards as Ambassador"](props.roomName, props.currentTurn, props.playerName, props.activePlayerID)}
-					disabled = {false}>Exchange your cards as Ambassador</Button>
+					disabled = {theRest}>Exchange your cards as Ambassador</Button>
 				</div>
 
 				<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 					<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves["Steal 2 from a player as Captain"](props.roomName, props.currentTurn, props.playerName, props.activePlayerID)}
-					disabled = {false}>Steal 2 from a player as Captain</Button>
+					disabled = {theRest}>Steal 2 from a player as Captain</Button>
 				</div>
 
 				<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 					<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves["Assassinate someone you dislike!"](props.roomName, props.currentTurn, props.playerName, props.activePlayerID)}
-					disabled = {false ? coins < 3 : true}>Assassinate someone you dislike!</Button>
+					disabled = {assassinEnabled}>Assassinate someone you dislike!</Button>
 				</div>
 
 				<div style ={{paddingBottom: "1em", paddingTop: "1em"}}>
 					<Button type="button" className="btn btn-lg btn-light" style = {{width:"20em"}} onClick={all_moves["Coup a scrub"](props.roomName, props.currentTurn, props.playerName, props.activePlayerID)}
-					disabled = {false ? coins < 3 : true}>Coup a scrub</Button>
+					disabled = {coupEnabled}>Coup a scrub</Button>
 				</div>
 	</div>);
 }
