e47d83b02634682a8a20b4501f9135fb38704bb0
Preet Shah <preettshah@hotmail.com>
2020-04-24 14:22:01 -0600

    assassin changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
+/*const firebaseConfig = {
     apiKey: "AIzaSyAM_2cfUP2u2pv55y_WgfnXrzO16l4RtVQ",
     authDomain: "coup-c4862.firebaseapp.com",
     databaseURL: "https://coup-c4862.firebaseio.com",
@@ -16,7 +16,21 @@ firebase.auth().signInAnonymously().catch(function(error) {
     // Handle Errors here.
     console.error(error);
 });
-firebase.analytics();
+firebase.analytics();*/
+var firebaseConfig = {
+    apiKey: "AIzaSyC5RhHMBsCsL8rnSJeDHha73_tayMsHHbQ",
+    authDomain: "couptest-5ae50.firebaseapp.com",
+    databaseURL: "https://couptest-5ae50.firebaseio.com",
+    projectId: "couptest-5ae50",
+    storageBucket: "couptest-5ae50.appspot.com",
+    messagingSenderId: "407017607899",
+    appId: "1:407017607899:web:3890f855bef489e6ab72fe",
+    measurementId: "G-8TMJ6V73P8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 
 export const firestore = firebase.firestore();
 // TODO: firestore room deletion still keeps data when same room name is reused
